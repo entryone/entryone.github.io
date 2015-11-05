43984b37e613006578b2c42e06a2c4ec5c904472
entryone <a.i.kulinich@gmail.com>
2015-11-05 16:50:43 +0200

    more complex state object

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 
-
 (function() {
     'use strict';
-
-
-
 
     //reducer
     function todos(state, action) {
         switch (action.type) {
             case 'ADD_TODO':
-                return state.concat([ action.text ]);
+                state.items.push({name: action.text });
+                return state;
             default:
                 return state
         }
     }
-
 
     //middleware
     function logger(_store) {
@@ -23,12 +19,12 @@
 
         return function (next) {
             return function (action) {
-                console.log('will dispatch', action);
+                console.log('will dispatch:', action);
 
                 // Call the next dispatch method in the middleware chain.
                 var returnValue = next(action);
 
-                console.log('state after dispatch', getState());
+                console.log('state after dispatch:', getState());
 
                 // This will likely be the action itself, unless
                 // a middleware further in chain changed it.
@@ -37,16 +33,10 @@
         };
     }
 
-
-
-
     //creating a store
     var createStoreWithMiddleware = Redux.applyMiddleware(logger)(Redux.createStore);
-    var store = createStoreWithMiddleware(todos, [ 'Use Redux' ]);
+    var store = createStoreWithMiddleware(todos, {items: []});
     //var store = Redux.createStore(todos, [ 'Use Redux' ]);
-
-
-
 
     angular.module('app', [])
         .controller('TodoListController', appController);
@@ -56,9 +46,7 @@
 
         var vm = this;
 
-
-        vm.list = store.getState();
-
+        vm.list = store.getState().items;
 
         vm.addTask = function(task){
             //dispatch store
@@ -70,12 +58,9 @@
             vm.task = '';
         };
 
-
-
         store.subscribe(function(state){
-            vm.list = store.getState();
+            vm.list = store.getState().items;
         });
-
 
     }
 
